5c086a5dafaf8f4db7fa2bdc55f7d9fe18a42079
Gabriel Silva <gabriel127.gk@gmail.com>
2023-01-29 14:33:40 -0300

    Ageitando responsividade - Info - Front-end

--- front-end/src/common/Button/index.tsx
+++ front-end/src/common/Button/index.tsx
@@ -7,15 +7,16 @@ interface IProps {
   weight?: string;
   font?: string;
   padding?: string;
+  position?: string;
 };
 
-export function Button ({ text, type, ariaLabel, bg, width, font, weight, padding }: IProps) {
+export function Button ({ text, type, ariaLabel, bg, width, font, weight, padding, position }: IProps) {
   
   return (
     <button
       aria-label={ariaLabel}
       type={type}
-      className={`${bg ? `${bg}` : "bg-primary-green"} ${width ? `${width}` : "w-72"} ${font ? `${font}` : "font-montserrat"} ${weight ? `${weight}` : "font-semibold"} ${padding ? `${padding}` : "p-2"} max-w-[10rem] hover:opacity-80 transition-all text-sm md:text-base text-primary-white leading-7 rounded-lg z-10`}>
+      className={`${bg ? `${bg}` : "bg-primary-green"} ${width ? `${width}` : "w-72"} ${font ? `${font}` : "font-montserrat"} ${weight ? `${weight}` : "font-semibold"} ${padding ? `${padding}` : "p-2"} ${position && position} max-w-[10rem] hover:opacity-80 transition-all text-sm md:text-base text-primary-white leading-7 rounded-lg z-10`}>
       {text}
     </button>
   );
--- front-end/src/components/Info/Info.tsx
+++ front-end/src/components/Info/Info.tsx
@@ -5,17 +5,17 @@ import { HeroContainer } from "./HeroContainer";
 export function Info () {
   return (
     <section className="flex flex-col items-center mt-10 sm:flex-row sm:gap-7 sm:h-80 ">
-      <div className="flex flex-col items-center sm:w-1/2">
+      <div className="flex flex-col items-center sm:items-start sm:ml-[3rem] lg:ml-[6rem]">
         <div>
-          <div className="font-montserrat font-bold leading-10 text-4xl md:text-5xl md:pl-2 lg:text-6xl lg:leading-[4rem] xl:mt-36">
+          <div className="font-montserrat font-bold leading-10 text-4xl md:text-5xl md:pl-2 lg:text-6xl lg:leading-[4rem] xl:leading-[5rem] xl:mt-36 xl:text-7xl">
             <h1>Organize</h1>
-            <h2 className="text-primary-green font-normal">your daily jobs</h2>
+            <h2 className="text-primary-green font-normal ">your daily jobs</h2>
           </div>
           <br />
           <p className="font-montserrat font-semibold leading-6 text-sm text-center sm:text-start">The only way to get things done</p>
           <br />
         </div>
-        <Button text="Go to To-do list" type="button" ariaLabel="Go to To-do list" />
+        <Button text="Go to To-do list" type="button" ariaLabel="Go to To-do list"/>
       </div>
       <div className="hidden sm:block">
         <HeroContainer />
