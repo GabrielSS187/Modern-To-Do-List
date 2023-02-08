0f321c347f6d8a0119076b688f67a8668bd8ec77
Gabriel Silva <gabriel127.gk@gmail.com>
2023-02-08 02:08:43 -0300

    Add. Middleware de autorização.

---
+++ back-end/src/middlewares/authMiddleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+
+import { JwtAdapter } from "../adapters/JwtAdapter/JwtAdapter";
+
+import { CustomError } from "../Errors/CustomError";
+
+const jwt = new JwtAdapter();
+
+export const authMiddleware = ( req: Request, res: Response, next: NextFunction ) => {
+  const { authorization } = req.headers;
+
+  if ( !authorization ) throw new Error("Token is required.");
+
+  const token = authorization.replace("Bearer", "").trim();
+
+  try {
+      const decoded = jwt.getTokenData({ token });
+
+      const { id } = decoded;
+      req.userId = id;
+
+      next();
+    } catch (error: any) {
+      return res.status(401).json(error.message);
+  };
+};
