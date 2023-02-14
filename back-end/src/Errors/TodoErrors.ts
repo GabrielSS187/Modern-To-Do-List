29f2fa0d956a3e3549e02a445fca126d21e3c083
Gabriel Silva <gabriel127.gk@gmail.com>
2023-02-14 16:15:55 -0300

    Ageitando algumas coisas no back. - 4

--- back-end/src/Errors/TodoErrors.ts
+++ back-end/src/Errors/TodoErrors.ts
@@ -1,4 +1,4 @@
-import { CustomError } from "./CustomError";
+import { CustomError } from "./customError";
 
 export class ErrorTodo extends CustomError {
   constructor(
--- back-end/src/Errors/userErrors.ts
+++ back-end/src/Errors/userErrors.ts
@@ -1,4 +1,4 @@
-import { CustomError } from "./CustomError";
+import { CustomError } from "./customError";
 
 export class ErrorUser extends CustomError {
   constructor(
