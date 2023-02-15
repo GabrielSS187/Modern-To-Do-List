852dc17fde9356e43b81f16a9c4027b5601b8372
Gabriel Silva <gabriel127.gk@gmail.com>
2023-02-15 12:47:28 -0300

    Add. Arquivos vercel - Back-end.

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -7,11 +7,10 @@ import { todoRoutes } from "./routes/todoRoutes";
 import { authMiddleware } from "./middlewares/authMiddleware";
 import { CustomError } from "./errors/CustomError";
 
-
 app.use("/user", userRoutes);
 app.use("/todo", authMiddleware, todoRoutes);
 
-//* Errors ============================================================
+//* Errors assíncronos ===================================================
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     return error instanceof CustomError 
   ?
