4bdebb18462cf8e5d81de47c4320f3c4723eda1a
Gabriel Silva <gabriel127.gk@gmail.com>
2025-04-13 22:49:21 -0300

    deleted

--- back-end/src/data/Database.ts
+++
@@ -1,8 +0,0 @@
-import knex from "knex";
-import knexConfig from "../config/knexfile";
-
-const environment = process.env.NODE_ENV || "development";
-
-export class Database {
- protected static connection = knex(knexConfig[environment]);
-};
